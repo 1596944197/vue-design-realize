af43ab0339a1491ced8f7d4f95c3bbda38a6e64d
mei_yiyi <1596944197@qq.com>
2023-02-26 13:09:40 +0800

    temp:临时保存

--- myReactive.ts
+++ myReactive.ts
@@ -8,9 +8,9 @@ export namespace MyReactive {
   /**
    * #### 用于存储响应式对象的键对应的副作用函数
    */
-  const Buckets = new Map<AnyObject, Map<keyof any, Set<Function>>>()
+  const Buckets = new WeakMap<AnyObject, Map<keyof any, Set<Function>>>()
 
-  let activeEffect: Function
+  let activeEffect
 
   const sourceObj = 'sourceObj'
 
@@ -28,12 +28,56 @@ export namespace MyReactive {
       }), {})
     }
   })()
+
+  const MapInterceptorFields = ['forEach', 'set']
+  const MapInterceptorHandler = (() => {
+    const handler = (method) => function l(this: AnyObject, ...args) {
+      const h = Map.prototype[method]
+      if (!h) return
+      return h.apply(this[sourceObj], args)
+    }
+    const forEach = function l(this: AnyObject, ...args) {
+      const cb = arguments[0]
+      const target = this[sourceObj]
+
+      target.forEach((v, k) => {
+        cb(wrap(v), wrap(k), this)
+      })
+    }
+    const set = function l(this: AnyObject, ...args) {
+      const [key, value] = arguments
+      const target: Map<any, any> = this[sourceObj]
+
+      if (!target.get(key)) track(target, key)
+
+      trigger(target, CurrentSetType['SET'])
+      return target.set(key, value)
+    }
+    return {
+      ...MapInterceptorFields.reduce((pre, cur) => ({
+        ...pre,
+        [cur]: handler(cur)
+      }), {}),
+      forEach,
+      set
+    }
+  })()
+
+
+  enum CurrentSetType {
+    ADD = "ADD",
+    SET = "SET",
+    DELETE = "DELETE",
+  }
   // ! 常量end
 
   const f1 = {
     a: 1
   }
-  const source = [f1]
+  const f2 = {
+    b: 2
+  }
+  const source = new Map([[f1, f2]])
 
   export function reactive<T extends AnyObject, O extends ReactiveOptions>(source: T, options?: O): ReactiveObject<T, O> {
     const proxy = new Proxy<T>(source, {
@@ -53,6 +97,10 @@ export namespace MyReactive {
 
         if (Array.isArray(target) && typeof p === 'string' && ArrayInterceptorFields.includes(p)) {
           return Reflect.get(ArrayInterceptorHandler, p, receiver)
+        }
+
+        if (getType(target) === 'map' && typeof p === 'string' && MapInterceptorFields.includes(p)) {
+          return Reflect.get(MapInterceptorHandler, p, receiver)
         }
 
         return value
@@ -76,7 +124,7 @@ export namespace MyReactive {
     if (!bucket) bucket = Buckets.set(target, new Map).get(target)!
     let keyList = bucket.get(p)
     if (!keyList) keyList = bucket.set(p, new Set).get(p)!
-    keyList.add(activeEffect)
+    activeEffect && keyList.add(activeEffect)
   }
 
   function trigger(target: AnyObject, p: keyof any, newValue?) {
@@ -88,8 +136,12 @@ export namespace MyReactive {
     if (Array.isArray(target) && p === 'length') {
       bucket.forEach((value, key) => {
         if (typeof key === 'symbol') return
-        if (key >= newValue) value.forEach(cb => callbackList.add(cb))
+        if (key >= newValue) value.forEach(cb => cb && callbackList.add(cb))
       })
+    }
+
+    if (getType(target) === 'map' && p === CurrentSetType['SET']) {
+      bucket.get('set')?.forEach(cb => cb && callbackList.add(cb))
     }
 
     callbackList.forEach(cb => cb())
@@ -99,15 +151,46 @@ export namespace MyReactive {
     return typeof v === 'object' && v !== null
   }
 
+  function wrap(v) {
+    return isObject(v) ? reactive(v) : v
+  }
+
   export function effect(callback: Function) {
     activeEffect = callback
     callback()
+    activeEffect = null
   }
 
   const r1 = reactive(source)
 
   effect(() => {
-    console.log(r1.includes(f1))
-    console.log(r1.indexOf(f1))
+    r1.forEach((v, k, t) => {
+      console.log(v)
+      console.log(k)
+    })
   })
+
+  r1.set(f1, {
+    b: 0xff
+  })
+}
+
+
+
+/**
+ * ### 获取数据的类型
+ * @date 2023/1/31 - 14:20:37
+ *
+ * @param {*} obj
+ * @returns {String}
+ */
+function getType(obj: any) {
+  let type = Object.prototype.toString
+    .call(obj)!
+    .match(/^\[object (.*)\]$/)![1]
+    .toLowerCase();
+  if (type === "string" && typeof obj === "object") return "object"; // Let "new String('')" return 'object'
+  if (obj === null) return "null"; // PhantomJS has type "DOMWindow" for null
+  if (obj === undefined) return "undefined"; // PhantomJS has type "DOMWindow" for undefined
+  return type;
 }
