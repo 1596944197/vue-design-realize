aed83caeb8abcc48d69e28cf3b18c2dbca821a51
1596944197 <1596944197@qq.com>
2023-02-27 10:16:10 +0800

    rewrite(map):map基本方法拦截

--- myReactive.ts
+++ myReactive.ts
@@ -8,61 +8,69 @@ export namespace MyReactive {
   /**
    * #### 用于存储响应式对象的键对应的副作用函数
    */
-  const Buckets = new WeakMap<AnyObject, Map<keyof any, Set<Function>>>()
+  const Buckets = new WeakMap<AnyObject, Map<keyof any, Set<Function>>>();
 
-  let activeEffect
+  let activeEffect;
 
-  const sourceObj = 'sourceObj'
+  const sourceObj = "sourceObj";
 
-  const ArrayInterceptorFields = ['includes', 'indexOf']
+  const ArrayInterceptorFields = ["includes", "indexOf"];
   const ArrayInterceptorHandler = (() => {
-    const handler = (method) => function l(this: AnyObject, ...args) {
-      const h = Array.prototype[method]
-      if (!h) return
-      return h.apply(this[sourceObj], args)
-    }
+    const handler = (method) =>
+      function l(this: AnyObject, ...args) {
+        const h = Array.prototype[method];
+        if (!h) return;
+        return h.apply(this[sourceObj], args);
+      };
     return {
-      ...ArrayInterceptorFields.reduce((pre, cur) => ({
-        ...pre,
-        [cur]: handler(cur)
-      }), {})
-    }
-  })()
+      ...ArrayInterceptorFields.reduce(
+        (pre, cur) => ({
+          ...pre,
+          [cur]: handler(cur),
+        }),
+        {}
+      ),
+    };
+  })();
 
-  const MapInterceptorFields = ['forEach', 'set']
+  const MapInterceptorFields = ["forEach", "set"];
   const MapInterceptorHandler = (() => {
-    const handler = (method) => function l(this: AnyObject, ...args) {
-      const h = Map.prototype[method]
-      if (!h) return
-      return h.apply(this[sourceObj], args)
-    }
+    const handler = (method) =>
+      function l(this: AnyObject, ...args) {
+        const h = Map.prototype[method];
+        if (!h) return;
+        return h.apply(this[sourceObj], args);
+      };
     const forEach = function l(this: AnyObject, ...args) {
-      const cb = arguments[0]
-      const target = this[sourceObj]
+      const cb = arguments[0];
+      const target = this[sourceObj];
 
       target.forEach((v, k) => {
-        cb(wrap(v), wrap(k), this)
-      })
-    }
+        cb(wrap(v), wrap(k), this);
+      });
+    };
     const set = function l(this: AnyObject, ...args) {
-      const [key, value] = arguments
-      const target: Map<any, any> = this[sourceObj]
+      const [key, value] = arguments;
+      const target: Map<any, any> = this[sourceObj];
 
-      if (!target.get(key)) track(target, key)
+      if (!target.has(key)) track(target, key);
+      const result = target.set(key, value);
 
-      trigger(target, CurrentSetType['SET'])
-      return target.set(key, value)
-    }
+      trigger(target, CurrentSetType["SET"]);
+      return result;
+    };
     return {
-      ...MapInterceptorFields.reduce((pre, cur) => ({
-        ...pre,
-        [cur]: handler(cur)
-      }), {}),
+      ...MapInterceptorFields.reduce(
+        (pre, cur) => ({
+          ...pre,
+          [cur]: handler(cur),
+        }),
+        {}
+      ),
       forEach,
-      set
-    }
-  })()
-
+      set,
+    };
+  })();
 
   enum CurrentSetType {
     ADD = "ADD",
@@ -71,111 +79,116 @@ export namespace MyReactive {
   }
   // ! 常量end
 
-  const f1 = {
-    a: 1
-  }
-  const f2 = {
-    b: 2
-  }
-  const source = new Map([[f1, f2]])
+  const source = new Map([
+    [1, 2],
+    [3, 4],
+  ]);
 
-  export function reactive<T extends AnyObject, O extends ReactiveOptions>(source: T, options?: O): ReactiveObject<T, O> {
+  export function reactive<T extends AnyObject, O extends ReactiveOptions>(
+    source: T,
+    options?: O
+  ): ReactiveObject<T, O> {
     const proxy = new Proxy<T>(source, {
       get(target, p, receiver) {
         if (p === sourceObj) {
-          return target
+          return target;
         }
-        track(target, p)
+        track(target, p);
 
-        const value = Reflect.get(target, p, receiver)
+        const value = Reflect.get(target, p, receiver);
 
         // # 如果只是浅响应-那就只响应一层，否则套一层再返回
         if (!options?.isShallow && isObject(value)) {
           // # 返回包装对象，不燃深响应毫无意义
-          return reactive(value, options)
+          return reactive(value, options);
         }
 
-        if (Array.isArray(target) && typeof p === 'string' && ArrayInterceptorFields.includes(p)) {
-          return Reflect.get(ArrayInterceptorHandler, p, receiver)
+        if (
+          Array.isArray(target) &&
+          typeof p === "string" &&
+          ArrayInterceptorFields.includes(p)
+        ) {
+          return Reflect.get(ArrayInterceptorHandler, p, receiver);
         }
 
-        if (getType(target) === 'map' && typeof p === 'string' && MapInterceptorFields.includes(p)) {
-          return Reflect.get(MapInterceptorHandler, p, receiver)
+        if (
+          getType(target) === "map" &&
+          typeof p === "string" &&
+          MapInterceptorFields.includes(p)
+        ) {
+          return Reflect.get(MapInterceptorHandler, p, receiver);
         }
 
-        return value
+        return value;
       },
       set(target, p, value, receiver) {
-        if (options?.isReadonly) return false
+        if (options?.isReadonly) return false;
 
-        const result = Reflect.set(target, p, value, receiver)
+        const result = Reflect.set(target, p, value, receiver);
 
-        if (p === 'length') trigger(target, p, value)
-        else trigger(target, p)
+        if (p === "length") trigger(target, p, value);
+        else trigger(target, p);
 
-        return result
+        return result;
       },
-    })
-    return proxy
+    });
+    return proxy;
   }
 
   function track(target: AnyObject, p: keyof any) {
-    let bucket = Buckets.get(target)
-    if (!bucket) bucket = Buckets.set(target, new Map).get(target)!
-    let keyList = bucket.get(p)
-    if (!keyList) keyList = bucket.set(p, new Set).get(p)!
-    activeEffect && keyList.add(activeEffect)
+    let bucket = Buckets.get(target);
+    if (!bucket) bucket = Buckets.set(target, new Map()).get(target)!;
+    let keyList = bucket.get(p);
+    if (!keyList) keyList = bucket.set(p, new Set()).get(p)!;
+    activeEffect && keyList.add(activeEffect);
   }
 
   function trigger(target: AnyObject, p: keyof any, newValue?) {
-    const bucket = Buckets.get(target)
+    const bucket = Buckets.get(target);
 
-    if (!bucket) return
-    let callbackList = bucket.get(p) ?? new Set
+    if (!bucket) return;
+    let callbackList = bucket.get(p) ?? new Set();
 
-    if (Array.isArray(target) && p === 'length') {
+    if (Array.isArray(target) && p === "length") {
       bucket.forEach((value, key) => {
-        if (typeof key === 'symbol') return
-        if (key >= newValue) value.forEach(cb => cb && callbackList.add(cb))
-      })
+        if (typeof key === "symbol") return;
+        if (key >= newValue) value.forEach((cb) => cb && callbackList.add(cb));
+      });
     }
 
-    if (getType(target) === 'map' && p === CurrentSetType['SET']) {
-      bucket.get('set')?.forEach(cb => cb && callbackList.add(cb))
+    if (getType(target) === "map" && p === CurrentSetType["SET"]) {
+      ["forEach", "set"].forEach((k) => {
+        bucket.get(k)?.forEach((cb) => cb && callbackList.add(cb));
+      });
     }
 
-    callbackList.forEach(cb => cb())
+    callbackList.forEach((cb) => cb());
   }
 
   function isObject(v) {
-    return typeof v === 'object' && v !== null
+    return typeof v === "object" && v !== null;
   }
 
   function wrap(v) {
-    return isObject(v) ? reactive(v) : v
+    return isObject(v) ? reactive(v) : v;
   }
 
   export function effect(callback: Function) {
-    activeEffect = callback
-    callback()
-    activeEffect = null
+    activeEffect = callback;
+    callback();
+    activeEffect = null;
   }
 
-  const r1 = reactive(source)
+  const r1 = reactive(source);
 
   effect(() => {
     r1.forEach((v, k, t) => {
-      console.log(v)
-      console.log(k)
-    })
-  })
-
-  r1.set(f1, {
-    b: 0xff
-  })
+      console.log(`${k}:${v}`);
+    });
+  });
+  // # map、set基本都需要重写方法
+  r1.set(0xff, 0x11);
 }
-
-
 
 /**
  * ### 获取数据的类型
